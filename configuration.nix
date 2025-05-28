c74679430563bc0a85cfd2cf2b1314d2a87980da
dosed-dev <dev.dozed@aleeas.com>
2025-05-29 02:58:00 +0300

    add iso

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
+    enabled = true;
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
--- flake.nix
+++ flake.nix
@@ -17,20 +17,34 @@
     }:
     {
       # nixos-anywhere --flake .#ionos-eu1 --generate-hardware-config nixos-facter facter.json <hostname>
-      nixosConfigurations.ionos-eu1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          nixos-facter-modules.nixosModules.facter
-          {
-            config.facter.reportPath =
-              if builtins.pathExists ./facter.json then
-                ./facter.json
-              else
-                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
-          }
-        ];
+      nixosConfigurations = {
+        ionos-eu1 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            ./configuration.nix
+            nixos-facter-modules.nixosModules.facter
+            {
+              config.facter.reportPath =
+                if builtins.pathExists ./facter.json then
+                  ./facter.json
+                else
+                  throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+            }
+          ];
+        };
+        iso-x86-64 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ({ modulesPath, ... }: {
+              imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+              services.openssh.enable = true;
+              users.users.root.openssh.authorizedKeys.keys = [
+                "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDjBF39he6/uZfi/1u4AFiXRpGMjLhphCAMeV+cw1O0bAAAABHNzaDo= Yubikey 5 NFC"
+              ];
+            })
+          ];
+        };
       };
     };
 }
