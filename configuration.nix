491343778b5bb67e2309896a828bf128f32d8061
dosed-dev <dev.dozed@aleeas.com>
2025-05-29 04:23:54 +0300

    fixup! fix typo

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
-    enabled = true;
+    enable = true;
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
