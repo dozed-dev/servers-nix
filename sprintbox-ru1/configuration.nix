5595d09b893d350fbe817c8f3a9b6747fc230891
dozed-dev <dev.dozed@aleeas.com>
2025-08-19 21:21:07 +0300

    add netwroking, enable zram

--- sprintbox-ru1/configuration.nix
+++ sprintbox-ru1/configuration.nix
@@ -5,26 +5,28 @@
   ...
 }:
 {
+  networking = {
+    interfaces.ens3.ipv4.addresses = [{
+      address = "185.185.70.168";
+      prefixLength = 22;
+    }];
+    defaultGateway = "185.185.68.1";
+    nameservers = ["141.8.194.254" "141.8.197.254"];
+  };
+
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/profiles/perlless.nix")
   ];
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    enable = true;
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
-  services.openssh.enable = true;
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
   ];
 
+  services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
-    # change this to your ssh key
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDjBF39he6/uZfi/1u4AFiXRpGMjLhphCAMeV+cw1O0bAAAABHNzaDo= Yubikey 5 NFC"
   ];
 
@@ -33,11 +35,7 @@
     size = 2 * 1024;
   }];
 
-  boot.kernelParams = [
-    "zswap.enabled=1"
-    "zswap.max_pool_percent=80"
-    "zswap.shrinker_enabled=1"
-  ];
+  zramSwap.enable = true;
 
   system.stateVersion = "25.05";
 }
