554f3c474140d4169a6293c735d6246246d33dcb
dozed-dev <dev.dozed@aleeas.com>
2025-08-20 03:08:08 +0300

    tmp remove

--- sprintbox-ru1/amnezia.nix
+++ sprintbox-ru1/amnezia.nix
@@ -9,26 +9,10 @@
   networking = {
     firewall = {
       enable = true;
-      allowedUDPPorts = [ 20158 ];
-    };
-    nat = {
-      enable = true;
-      internalInterfaces = [ "awg0" ];
-      externalInterface = "ens3";
+      allowedUDPPorts = [ 51820 ];
     };
     wg-quick.interfaces = {
-      awg0 = {
-        address = [ "10.100.0.1/24" "fdc9:281f:04d7::1/64" ];
-        listenPort = 20158;
-        privateKeyFile = "/root/awg0.key"; # LfSV6/ei/tmaKmqqC6cakChlrMRFJ8i7kOtxHpIXlXY=
-
-        peers = [
-          { # lappy
-            publicKey = "OiO3aBDjsCxQYkEr4lqyKA1o1zd0Ddq8U7K72YbUAgk=";
-            allowedIPs = [ "10.100.0.2/32" "fdc9:281f:04d7::2/80" ];
-          }
-        ];
-      };
+      awg0.configFile = "/root/awg0.conf";
     };
   };
 }
